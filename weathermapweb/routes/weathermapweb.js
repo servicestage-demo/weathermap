62c83bb0ac154fc413129b35cc24421c00ecae38
zqfang666 <847368807@qq.com>
2023-03-07 22:21:43 +0800

    1.springcloudhw框架天气预报demo，解除Mesher依赖，引入edge-service；2.升级框架版本，支持仪表盘显示

--- weathermapweb/routes/weathermapweb.js
+++ weathermapweb/routes/weathermapweb.js
@@ -7,8 +7,19 @@ router.get('/', function (req, res, next) {
     var proxy_host = process.env.HTTP_PROXY_HOST || '127.0.0.1';
     var proxy_port = process.env.HTTP_PROXY_PORT || '30101';
     var service_addr = process.env.SERVICE_ADDR || 'fusionweather'
+
+    // Adapt to the scenario without Mesher.You only need to configure the SERVICE_ADDR
+    if (service_addr.split(":").length === 2
+        && typeof (process.env.HTTP_PROXY_HOST) == 'undefined'
+        && typeof (process.env.HTTP_PROXY_PORT) == 'undefined') {
+        proxy_host = service_addr.split(":")[0];
+        proxy_port = service_addr.split(":")[1];
+    }
+
+    console.log('service_addr is ' + service_addr);
     console.log(proxy_host + ':' + proxy_port);
     console.log(req.query.city + ", " + req.query.type);
+
     var opt = {
         host: proxy_host,
         port: proxy_port,
